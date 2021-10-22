cc3b4ed41f634550b27fc09207f212acd1f679c5
soulpower11 <5299979+soulpower11@users.noreply.github.com>
2021-10-22 16:26:58 +0800

    updating ordermgr

--- src/OrderMgr.java
+++ src/OrderMgr.java
@@ -208,8 +208,82 @@ public class OrderMgr {
 		}
 	}
 
-	public void generateSalesReport(List<Order> Orders, int Integar) {
+public void addOrderItem(List<Order> order, List<MenuItem> MenuItem, List<Promotion> Promotion, int tableNumber) {
+		int orderSize = order.size();
+		Order found = null;
+		Scanner sc = new Scanner(System.in);
+		int choice = -1;
 
+		for (int i = 0; i < orderSize; i++) {
+			if (order.get(i).getTableNumber() == tableNumber) {
+				found = order.get(i);
+				break;
+			}
+		}
+		if (found != null) {
+			while (choice != 0) {
+				System.out.println("(1) Add Ala Carte Items");
+				System.out.println("(2) Add Promotions Items");
+				System.out.println("(3) Done/Cancel Adding Items");
+
+				System.out.printf("Enter the number of your choice: ");
+				choice = sc.nextInt();
+				switch (choice) {
+				case 1:
+					int mLength = MenuItem.size();
+					System.out.println("CHOICE NAME		FOOD CATEGORY		DESCRIPTION		PRICE");
+					for (int i = 0; i < mLength; i++) {
+						System.out.println("(" + (i + 1) + ") " + MenuItem.get(i).getName() + "		"
+								+ MenuItem.get(i).getFoodCategory() + "		" + MenuItem.get(i).getDescription()
+								+ "		$" + String.format("%.2f", MenuItem.get(i).getPrice()));
+					}
+
+					System.out.println("(0) Go back to selection");
+					System.out.printf("Enter the number of your choice: ");
+					choice = sc.nextInt();
+
+					while (choice != 0) {
+						found.addOrderItem(MenuItem.get(choice - 1));
+						System.out.println(MenuItem.get(choice - 1).getName() + " Added!");
+
+						System.out.printf("Enter the number of your choice: ");
+						choice = sc.nextInt();
+					}
+					choice = 1;
+					break;
+				case 2:
+					int pLength = Promotion.size();
+
+					System.out.println("CHOICE NAME		DESCRIPTION		PRICE");
+					for (int i = 0; i < pLength; i++) {
+						System.out.println("(" + (i + 1) + ") " + Promotion.get(i).getName() + "		"
+								+ Promotion.get(i).getDescription() + "				"
+								+ String.format("%.2f", Promotion.get(i).getPrice()));
+					}
+
+					System.out.println("(0) Go back to selection");
+					System.out.printf("Enter the number of your choice: ");
+					choice = sc.nextInt();
+
+					while (choice != 0) {
+						found.addOrderItem(Promotion.get(choice - 1));
+						System.out.println(Promotion.get(choice - 1).getName() + " Added!");
+						System.out.printf("Enter the number of your choice: ");
+						choice = sc.nextInt();
+					}
+					choice = 1;
+					break;
+				case 3:
+					choice = 0;
+					break;
+				default:
+					System.out.println("Invalid Choice!");
+					break;
+				}
+			}
+		} else {
+			System.out.println("Order not found!");
+		}
 	}
 
 }
